c335e3bab51751e0f711c23abe3dafa27bcf2de8
Itzik.Jan <itzik.jan@perion.com>
2019-02-28 13:18:54 +0200

    change version setup to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'join_skew_data',         # How you named your package folder (MyLib)
   packages = ['join_skew_data'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='Undertone',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'customize partitions with skew data for join',   # Give a short description about your library
   author = 'itzik jan',                   # Type in your name
