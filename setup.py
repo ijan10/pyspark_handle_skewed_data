2f58647a456cb80bb4973d5db4222d53f120ef24
Itzik.Jan <itzik.jan@perion.com>
2019-02-28 13:21:24 +0200

    change version setup to 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'itzik jan',                   # Type in your name
   author_email = 'itzikjan@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ijan10/pyspark_handle_skewed_data',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ijan10/pyspark_handle_skewed_data/archive/v_0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ijan10/pyspark_handle_skewed_data/archive/v_0.2.tar.gz',    # I explain this later on
   keywords = ['PySpark', 'skew', 'join'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'operator'
